ed0e9ec5520667e8e565113f4e5d9a7f69955140
Peter Milne <peter.milne@d-tacq.com>
2025-04-27 16:11:44 +0100

    set uut mode at the outset:a ACQ400_PACK24[=1] ACQ400_SOFT_TRIGGER[=1]
     On branch main
    	modified:   acq400_chapi/acq400_stream.cpp

--- acq400_chapi/acq400_stream.cpp
+++ acq400_chapi/acq400_stream.cpp
@@ -41,6 +41,37 @@ int streamer(acq400_chapi::Acq400& uut, acq400_chapi::Ports port)
 	return 0;
 }
 
+void set_data_mode(acq400_chapi::Acq400& uut, int pack24)
+{
+	int rc = uut.set("1", "pack24", pack24);
+	assert(rc == 0);
+}
+
+#define ENABLE 1
+#define RISING 1
+
+void set_trigger_mode(acq400_chapi::Acq400& uut, int soft_trigger)
+{
+	std::string response;
+	int rc = uut.set(response, "1", "trg=%d,%d,%d", ENABLE, soft_trigger, RISING);
+	assert(rc == 0);
+}
+
+void set_uut_mode(acq400_chapi::Acq400& uut){
+	int pack24 = 0;
+	if (getenv("ACQ400_PACK24")){
+		pack24 = atoi(getenv("ACQ400_PACK24"));
+	}
+	set_data_mode(uut, pack24);
+
+	int soft_trigger = 0;
+	if (getenv("ACQ400_SOFT_TRIGGER")){
+		soft_trigger = atoi(getenv("ACQ400_SOFT_TRIGGER"));
+	}
+
+	set_trigger_mode(uut, soft_trigger);
+}
+
 int main(int argc, char **argv) {
 	if (argc < 2){
 		fprintf(stderr, "USAGE: acq400_stream UUT [PORT]\n");
@@ -52,6 +83,7 @@ int main(int argc, char **argv) {
 	if (getenv("ACQ400_STREAM_MAXM")){
 		G::maxbytes = strtoull(getenv("ACQ400_STREAM_MAXM"), 0, 0) * 0x100000;
 	}
+
 	const char* host = argv[1];
 	acq400_chapi::Ports port = acq400_chapi::STREAM;
 	if (argc > 2){
@@ -59,11 +91,15 @@ int main(int argc, char **argv) {
 	}
 
 	acq400_chapi::Acq400 uut(host);
+
+	set_uut_mode(uut);
+
 	int data32;
 	if (uut.get("0", "data32", data32) < 0){
 		fprintf(stderr, "ERROR:");
 		exit(1);
 	}
+
 	if (data32){
 		return streamer<long>(uut, port);
 	}else{
