983dfe75ab6b5a9ebf203a0e71c99527c65ff244
Peter Milne <peter.milne@d-tacq.com>
2025-05-21 19:35:40 +0100

    forcing hard trigger not helpful
     Changes to be committed:
    	modified:   acq400_chapi/acq400_load_awg_seg.cpp

--- acq400_chapi/acq400_load_awg_seg.cpp
+++ acq400_chapi/acq400_load_awg_seg.cpp
@@ -156,9 +156,9 @@ void disable_fp_trigger(acq400_chapi::Acq400& uut)
 void restore_fp_trigger(acq400_chapi::Acq400& uut){
 	std::string response;
 	std::string site1 = "1";
-	uut.set(response, site1, "trg=1,0,1");
+//	uut.set(response, site1, "trg=1,0,1");
 	std::string site0 = "0";
-	uut.set(response, site0, "%s %s", TRG_SRC_0, "EXT");
+	uut.set(response, site0, "%s %s", TRG_SRC_0, G_trigger_stash);
 	printf("restore_fp_trigger() restore \"%s\" to \"%s\"\n",
 			TRG_SRC_0, G_trigger_stash);
 }
