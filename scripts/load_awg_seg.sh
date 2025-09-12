4c5d6b5b5ee49dfa079eaa8966a0e4460621fe58
sambelltacq <Sam.Bell@d-tacq.co.uk>
2025-09-12 12:40:06 +0100

    Now handles up to 26 segments 	modified:   scripts/load_awg_seg.sh

--- scripts/load_awg_seg.sh
+++ scripts/load_awg_seg.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-PATH=./acq400_chapi/:$PATH
+# Usage:
+#       ./load_awg_seg.sh acq1102_010 pattern-1.dat pattern-2.dat
+#       ./load_awg_seg.sh acq1102_010 pattern-*.dat
+#       ACQ400_LAS_SWITCH_SEG=1000 NFILES=2 ./load_awg_seg.sh acq1102_010 pattern-*.dat
 
-# Usage ./send_segs.sh acq1102_010 id32x1-64-193600-*
 uut="$1"
+if ! ping -c 1 "$1" &> /dev/null; then
+  echo "Unable to connect to '$1'"
+  exit 1
+fi
 
 shift
-if [[ "$*" == *[*?]* ]]; then
-  files=($(ls $@ | sort))
-else
-  files=($@)
-fi
+files=( "$@" )
+NFILES=${NFILES:-26}
+LETTERS=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 
 export ACQ400_LAS_MODE=${ACQ400_LAS_MODE:-ARP}
 export ACQ400_LAS_SWITCH_SEG=${ACQ400_LAS_SWITCH_SEG:-1000}
 
-if [ ${#files[@]} -gt 0 ]; then
-  cmd="acq400_load_awg_seg ${uut}"
-  [ -n "${files[0]}" ] && cmd="$cmd A=${files[0]}"
-  [ -n "${files[1]}" ] && cmd="$cmd B=${files[1]}"
-  [ -n "${files[2]}" ] && cmd="$cmd C=${files[2]}"
-  [ -n "${files[3]}" ] && cmd="$cmd D=${files[3]}"
-  [ -n "${files[4]}" ] && cmd="$cmd E=${files[4]}"
-  echo $cmd
-  eval $cmd
-fi
+ROOT_DIR=$(dirname $(realpath "$(dirname "$0")"))
+cmd="${ROOT_DIR}/acq400_chapi/acq400_load_awg_seg ${uut}"
+
+for file in "${files[@]}"; do
+  (( count >= NFILES )) && break
+  (( count >= ${#LETTERS[@]} )) && break
+  cmd="$cmd ${LETTERS[count]}=$file"
+  ((count++))
+done
+
+echo $cmd
+$cmd
 
